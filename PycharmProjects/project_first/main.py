4f8895fe467ad754f2cb557b024c54148365790a
Shpunka <mifaizullin@edu.hse.ru>
2023-11-16 21:45:20 +0300

    main modifed

--- PycharmProjects/project_first/main.py
+++ PycharmProjects/project_first/main.py
@@ -1,25 +1,35 @@
 import pygame as pg
 import sys
 from utils.settings import *
+from sprites.player import Player
 
 pg.init()
 
+bg = (255, 255, 255)
 screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+player = Player(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+all_sprites = pg.sprite.Group(player)
+
+
+
 def main():
-    run = True
-    while run:
+    while True:
+        screen.fill(bg)
+        all_sprites.draw(screen)
         clock = pg.time.Clock()
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                run = False
                 pg.quit()
                 sys.exit()
+
+        all_sprites.update()
 
         pg.display.flip()
 
         clock.tick(60)
+
 
 if __name__ == '__main__':
     main()
