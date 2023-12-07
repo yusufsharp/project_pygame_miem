d59663fdc76492a53a04aae5b125d64bffc0d196
Yusuf <zxces@vk.com>
2023-12-08 02:16:22 +0300

    Добавил проверку пароля, оптимизировал код

--- PycharmProjects/project_first/menu.py
+++ PycharmProjects/project_first/menu.py
@@ -12,19 +12,34 @@ def send_post_request(username, password):
     print(response.text)
 
 
-def send_get_request(username):
-    url = "https://zxces.pythonanywhere.com/myapp/api/player_info/"
-    params = {"login": username}
+def send_get_request(username, password):
+    url = f"https://zxces.pythonanywhere.com/api/player_info/{username}"
+    params = {"login": username, "password": password}
     response = requests.get(url, params=params)
     if response.status_code == 200:
         player_info = response.json()
         print("Игрок найден. Информация о игроке:", player_info)
+        print(response.text)
         return True
     elif response.status_code == 404:
         print("Игрок не найден.")
+        print(response.text)
         return False
     else:
         print(f"Произошла ошибка: {response.status_code}, {response.text}")
+        return False
+
+
+def register_request(username, password):
+    url = f"https://zxces.pythonanywhere.com/register/{username}/{password}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        player_info = response.json()
+        print("Игрок зарегестрирован", player_info)
+        print(response.text)
+        return True
+    elif response.status_code == 401:
+        print(f"Неверный пароль: {response.status_code}, {response.text}")
         return False
 
 
@@ -41,17 +56,8 @@ def menuFunc():
 
         login_button_rect = draw_rect(screen, 35, 20, 30, 10)
         register_button_rect = draw_rect(screen, 35, 40, 30, 10)
-
-        login_text = font.render("Войти", True, (0, 0, 0))
-        register_text = font.render("Рейтинг", True, (0, 0, 0))
-        login_text_rect = login_text.get_rect()
-        register_text_rect = register_text.get_rect()
-        login_text_rect.center = ((login_button_rect.right + login_button_rect.left) // 2,
-                                  (login_button_rect.bottom + login_button_rect.top) // 2)
-        register_text_rect.center = ((register_button_rect.right + register_button_rect.left) // 2,
-                                     (register_button_rect.bottom + register_button_rect.top) // 2)
-        screen.blit(login_text, login_text_rect)
-        screen.blit(register_text, register_text_rect)
+        print_text_in_bar(screen, font, 'Войти', login_button_rect, clr=(0, 0, 0))
+        print_text_in_bar(screen, font, 'Рейтинг', register_button_rect, clr=(0, 0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -70,43 +76,17 @@ def menuFunc():
     return reg
 
 
-def draw_rect(screen, x, y, width, height, clr=(255, 255, 255), border=20, border_width=0):
-    window_x = (WINDOW_WIDTH * x) // 100
-    window_y = (WINDOW_HEIGHT * y) // 100
-    window_width = (WINDOW_WIDTH * width) // 100
-    window_height = (WINDOW_HEIGHT * height) // 100
-    n_rect = pg.Rect(window_x, window_y, window_width, window_height)
-    pygame.draw.rect(screen, clr, n_rect, border_radius=border, width=border_width)
-    return n_rect
-
-
 def menu_reg_func(font, screen):
-    input_active = True
-    reg_window = draw_rect(screen, 20, 20, 60, 50, clr=(255, 255, 255, 128))
+    input_active = True  # переменная которая отвечает за прогонку меню окна регистрации
+    draw_rect(screen, 20, 20, 60, 50, clr=(255, 255, 255, 128))
 
     # треба настроить прозрачность
     name_window = draw_rect(screen, 30, 30, 40, 10, clr=(0, 0, 0), border_width=2)
     pass_window = draw_rect(screen, 30, 45, 40, 10, clr=(0, 0, 0), border_width=2)
     init_window = draw_rect(screen, 45, 60, 10, 5, clr=(255, 0, 0), border_width=3)
-
-    name_text = font.render("Логин: ", True, (200, 200, 200))
-    pass_text = font.render("Пароль: ", True, (200, 200, 200))
-    init_text = font.render("Играть", True, (0, 0, 0))
-    # здесь задаем тексты логина пароля
-    name_text_rect = name_text.get_rect()
-    pass_text_rect = pass_text.get_rect()
-    init_text_rect = init_text.get_rect()
-    name_text_rect.center = ((name_window.right + name_window.left) // 2,
-                             (name_window.bottom + name_window.top) // 2)
-    pass_text_rect.center = ((pass_window.right + pass_window.left) // 2,
-                             (pass_window.bottom + pass_window.top) // 2)
-    init_text_rect.center = ((init_window.right + init_window.left + 2) // 2,
-                             (init_window.bottom + init_window.top - 6) // 2)
-
-    screen.blit(name_text, name_text_rect)
-    screen.blit(pass_text, pass_text_rect)
-    screen.blit(init_text, init_text_rect)
-    # для активации поля ввода
+    name_text_rect = print_text_in_bar(screen, font, "Логин: ", name_window)
+    pass_text_rect = print_text_in_bar(screen, font, "Пароль: ", pass_window)
+    print_text_in_bar(screen, font, "Играть", init_window, right_pos=2, bottom_pos=-6)
 
     active_name = False
     active_pass = False
@@ -118,7 +98,8 @@ def menu_reg_func(font, screen):
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                input_active = False
+                pygame.quit()
+                sys.exit()
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if name_window.collidepoint(event.pos):
@@ -133,13 +114,19 @@ def menu_reg_func(font, screen):
                     pygame.draw.rect(screen, (0, 0, 0), name_window, 2, border_radius=20)
                 elif init_window.collidepoint(event.pos):
                     if text_name != '' and text_pass != '':
-                        user_exists = send_get_request(text_name)
+                        user_exists = send_get_request(text_name, text_pass)
                         if not user_exists:
                             send_post_request(text_name, text_pass)
                             print('Игрок проходит регистрацию')
+                            input_active = False
                         else:
-                            print('Игрок уже существует')
-                        input_active = False
+                            auth = register_request(text_name, text_pass)
+                            if not auth:
+                                error_rect = draw_rect(screen, 35, 20, 30, 10)
+                                print_text_in_bar(screen, font, 'Неверный пароль!', error_rect, clr=(255, 0, 0))
+                            else:
+                                print('Добро пожаловать!!!')
+                                input_active = False
 
             elif event.type == pygame.KEYDOWN:
                 if active_name:
@@ -150,6 +137,27 @@ def menu_reg_func(font, screen):
                 if text_name != '' and text_pass != '':
                     pygame.draw.rect(screen, (0, 255, 0), init_window, 3, border_radius=20)
         pygame.display.flip()
+
+
+def draw_rect(screen, x, y, width, height, clr=(255, 255, 255), border=20, border_width=0):
+    window_x = (WINDOW_WIDTH * x) // 100
+    window_y = (WINDOW_HEIGHT * y) // 100
+    window_width = (WINDOW_WIDTH * width) // 100
+    window_height = (WINDOW_HEIGHT * height) // 100
+    n_rect = pg.Rect(window_x, window_y, window_width, window_height)
+    pygame.draw.rect(screen, clr, n_rect, border_radius=border, width=border_width)
+    return n_rect
+
+
+def print_text_in_bar(screen, font, text, bar, right_pos=0, bottom_pos=0, clr=(200, 200, 200)):
+    bar_text = font.render(f"{text}", True, clr)
+
+    bar_text_rect = bar_text.get_rect()
+    bar_text_rect.center = ((bar.right + bar.left + right_pos) // 2,
+                            (bar.bottom + bar.top + bottom_pos) // 2)
+
+    screen.blit(bar_text, bar_text_rect)
+    return bar_text_rect
 
 
 def text_bar_updating(screen, event, font, window, text, text_rect):
@@ -168,3 +176,4 @@ def text_bar_updating(screen, event, font, window, text, text_rect):
     screen.blit(text_surface, text_surface_rect)
 
     return text
+
