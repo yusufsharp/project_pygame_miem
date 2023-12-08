ed7132c193860eda74c64c02ef9a49a6e61a55bf
Yusuf <zxces@vk.com>
2023-12-09 00:33:13 +0300

    Эпичное интро и комментарии

--- PycharmProjects/project_first/menu.py
+++ PycharmProjects/project_first/menu.py
@@ -10,7 +10,7 @@ def send_post_request(username, password):
     url = "https://zxces.pythonanywhere.com/myapp/api/player_info/"
     data = {"login": username, "password": password}
     response = requests.post(url, data=data)
-    print(response.text)
+    print(response.text)  # берет всю бд по адресу
 
 
 def send_get_request(username, password):
@@ -32,7 +32,7 @@ def send_get_request(username, password):
 
 
 def register_request(username, password):
-    url = f"https://zxces.pythonanywhere.com/register/{username}/{password}"
+    url = f"https://zxces.pythonanywhere.com/register/{username}/{password}"  # генерит ссылку логин и пароль
     response = requests.get(url)
     if response.status_code == 200:
         player_info = response.json()
@@ -45,21 +45,21 @@ def register_request(username, password):
 
 
 def menuFunc():
-    reg = False
-    clock = pygame.time.Clock()
+    reg = False  # отвечает за отображение всего меню
+    clock = pygame.time.Clock()  # фпс
     background_image = pg.image.load('images/Background.png')
-    scaled_image = pg.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+    scaled_image = pg.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))  # подгоняем изображение
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    screen.blit(scaled_image, (0, 0))
+    screen.blit(scaled_image, (0, 0))  # отображаем изображение
     font = pg.font.Font('fonts/thin_pixel-7.ttf', 36)
-    animate(screen, 35, 20, 30, 10, duration=200)
+    animate(screen, 35, 20, 30, 10)  # анимация появления логин
     login_button_rect = draw_rect(screen, 35, 20, 30, 10)
     print_text_in_bar(screen, font, 'Войти', login_button_rect, clr=(0, 0, 0))
-    animate(screen, 35, 40, 30, 10, duration=200)
+    animate(screen, 35, 40, 30, 10)  # анимация появления рейтинг
     register_button_rect = draw_rect(screen, 35, 40, 30, 10)
     print_text_in_bar(screen, font, 'Рейтинг', register_button_rect, clr=(0, 0, 0))
     while not reg:
-
+        # главный цикл отображает запускает все остальное
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -91,62 +91,62 @@ def menu_reg_func(font, screen):
     pass_text_rect = print_text_in_bar(screen, font, "Пароль: ", pass_window)
     print_text_in_bar(screen, font, "Играть", init_window, right_pos=2, bottom_pos=-6, clr=(0, 0, 0))
 
-    active_name = False
-    active_pass = False
-    text_name = ''
+    active_name = False  # нажатие на окно логина
+    active_pass = False  # нажатие на окно пароля
+    text_name = ''  # сам текст
     text_pass = ''
 
     while input_active:
         pygame.font.init()
 
-        for event in pygame.event.get():
+        for event in pygame.event.get():  # анализ клавиатуры
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if name_window.collidepoint(event.pos):
-                    active_name = not active_name
+                    active_name = True
                     active_pass = False
-                    pygame.draw.rect(screen, (0, 255, 0), name_window, 2, border_radius=20)
-                    pygame.draw.rect(screen, (0, 0, 0), pass_window, 2, border_radius=20)
+                    pygame.draw.rect(screen, (0, 255, 0), name_window, 2, border_radius=20)  # меняет цвет рамки логин
+                    pygame.draw.rect(screen, (0, 0, 0), pass_window, 2, border_radius=20)  # меняет цвет пароль
                 elif pass_window.collidepoint(event.pos):
-                    active_pass = not active_pass
+                    active_pass = True
                     active_name = False
                     pygame.draw.rect(screen, (0, 255, 0), pass_window, 2, border_radius=20)
                     pygame.draw.rect(screen, (0, 0, 0), name_window, 2, border_radius=20)
-                elif init_window.collidepoint(event.pos):
-                    if text_name != '' and text_pass != '':
-                        pygame.draw.rect(screen, (255, 255, 255), init_window)
-                        animate(screen, 45, 60, 10, 5, clr=(0, 255, 0))
-                        user_exists = send_get_request(text_name, text_pass)
+                elif init_window.collidepoint(event.pos):  # нажатие на кнопку играть
+                    if text_name != '' and text_pass != '':  # не пустые рамки с текстом
+                        pygame.draw.rect(screen, (255, 255, 255), init_window)  # отбеливание батона
+                        animate(screen, 45, 60, 10, 5, clr=(0, 255, 0))  # зеленый батон загрузки
+                        user_exists = send_get_request(text_name, text_pass)  # проверка на регу игрока
                         if not user_exists:
-                            pygame.draw.rect(screen, (255, 255, 255), init_window)
-                            animate(screen, 45, 60, 10, 5, clr=(200, 200, 200))
-                            send_post_request(text_name, text_pass)
+                            pygame.draw.rect(screen, (255, 255, 255), init_window)  # отбеливание батона
+                            animate(screen, 45, 60, 10, 5, clr=(200, 200, 200))  # серый батон загрузки
+                            send_post_request(text_name, text_pass)  # передает данные для реги
                             print('Игрок проходит регистрацию')
-                            input_active = False
+                            input_active = False  # закончили арку меню
                         else:
-                            auth = register_request(text_name, text_pass)
+                            auth = register_request(text_name, text_pass)  # проверка пароля
                             if not auth:
-                                pygame.draw.rect(screen, (255, 255, 255), init_window)
-                                animate(screen, 45, 60, 10, 5, clr=(255, 0, 0))
+                                pygame.draw.rect(screen, (255, 255, 255), init_window)  # отбеливание батона
+                                animate(screen, 45, 60, 10, 5, clr=(255, 0, 0))  # красный батон загрузки
                                 error_rect = draw_rect(screen, 35, 20, 30, 10)
                                 print_text_in_bar(screen, font, 'Неверный пароль!', error_rect, clr=(255, 0, 0))
                             else:
-                                pygame.draw.rect(screen, (255, 255, 255), init_window)
-                                animate(screen, 45, 60, 10, 5, clr=pygame.Color('dodgerblue2'))
+                                pygame.draw.rect(screen, (255, 255, 255), init_window)  # отбеливание батона
+                                animate(screen, 45, 60, 10, 5, clr=pygame.Color('dodgerblue2'))  # синий батон загрузки
                                 print('Добро пожаловать!!!')
                                 input_active = False
 
-            elif event.type == pygame.KEYDOWN:
-                if active_name:
+            elif event.type == pygame.KEYDOWN:  # любая клавиша текста
+                if active_name:  # функция обновляет и центрирует текст
                     text_name = text_bar_updating(screen, event, font, name_window, text_name, name_text_rect)
                 elif active_pass:
                     text_pass = text_bar_updating(screen, event, font, pass_window, text_pass, pass_text_rect)
 
                 if text_name != '' and text_pass != '':
-                    pygame.draw.rect(screen, (255, 255, 255), init_window, border_radius=20)
+                    pygame.draw.rect(screen, (255, 255, 255), init_window, border_radius=20)  # делает зеленую рамку
                     print_text_in_bar(screen, font, "Играть", init_window, right_pos=2, bottom_pos=-6, clr=(0, 0, 0))
                     pygame.draw.rect(screen, (0, 255, 0), init_window, 3, border_radius=20)
         pygame.display.flip()
@@ -191,7 +191,7 @@ def text_bar_updating(screen, event, font, window, text, text_rect):
     return text
 
 
-def animate(screen, x, y, width, height, clr=(255, 255, 255), border=20, border_width=0, duration=300):
+def animate(screen, x, y, width, height, clr=(255, 255, 255), border=20, border_width=0, duration=150):
     start_time = pg.time.get_ticks()
     while True:
         elapsed_time = pg.time.get_ticks() - start_time
@@ -203,7 +203,7 @@ def animate(screen, x, y, width, height, clr=(255, 255, 255), border=20, border_
         window_x = (WINDOW_WIDTH * x) // 100
         window_y = (WINDOW_HEIGHT * y) // 100
 
-        window_width = int((WINDOW_WIDTH * width) / 100 * (progress ** (1 / 3)))
+        window_width = int((WINDOW_WIDTH * width) / 100 * (progress ** (1 / 3)))  # нелинейное отображение корнем куба
         window_height = (WINDOW_HEIGHT * height) // 100
         n_rect = pg.Rect(window_x, window_y, window_width, window_height)
         pygame.draw.rect(screen, clr, n_rect, border_radius=border, width=border_width)
@@ -212,28 +212,31 @@ def animate(screen, x, y, width, height, clr=(255, 255, 255), border=20, border_
                 pg.quit()
                 sys.exit()
 
-        pg.time.Clock().tick(60)
-        pygame.display.flip()
+        pg.time.Clock().tick(60)  # фпс
+        pygame.display.flip()  # обновление экрана
+
 
 def darken_screen(screen, duration=3000):
     overlay = pg.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
-    overlay.fill((0, 0, 0))  # Задаем черный цвет
+    overlay.fill((0, 0, 0))  # задаем черный цвет, чистим экран
     alpha = 0
-    font = pg.font.Font('fonts/thin_pixel-7.ttf', 108)
     start_time = pg.time.get_ticks()
 
     while alpha <= 255:
         elapsed_time = pg.time.get_ticks() - start_time
         if elapsed_time >= duration:
             break
-        progress = math.sin((elapsed_time / duration) * math.pi / 2)
+        progress = math.sin((elapsed_time / duration) * math.pi / 2)  # нелинейное отображение синусом
+
+        font = pg.font.Font('fonts/thin_pixel-7.ttf', int(32 + progress * 404))  # изменяем размер шрифта
         alpha = int(progress * 255)
-        overlay.set_alpha(alpha)
+        overlay.set_alpha(alpha)  # просвет на поверхность
         radius = progress * WINDOW_WIDTH
         alpha = int((1 - progress) * 255)
-        screen.blit(overlay, (0, 0))  # Наложение затемненной поверхности на экран
+        screen.blit(overlay, (0, 0))  # наложение затемненной поверхности на экран
         pg.draw.circle(overlay, (200, 0, 0, alpha), (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2), int(radius))
-        print_text_in_bar(screen, font, 'Real Hero', overlay.get_rect(), clr=(255, 255, 255, alpha))
+        overlay_rect = overlay.get_rect()
+        print_text_in_bar(screen, font, 'Real Hero', overlay_rect, clr=(255, 255, 255))
         pg.display.flip()
 
         for event in pg.event.get():
