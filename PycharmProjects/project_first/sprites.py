91f4a274c0d5f96ae8bfd4fff26a90a1d868caab
Shpunka <mifaizullin@edu.hse.ru>
2023-12-16 09:39:50 +0300

    FINAL FINAL BUH

--- PycharmProjects/project_first/sprites.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-class Ground(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("media/images/Ground.png")
-        self.rect = pygame.image.get_rect(center=(350, 350))
